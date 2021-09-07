11ad8bd1a8e62bb4597ed47f67ebb9039b861112
Nickothan <s1nicolasrg@gmail.com>
2021-09-07 17:49:45 -0500

    [MOW-7] styles from Button

--- src/components/shared/Button/styles.ts
+++ src/components/shared/Button/styles.ts
@@ -1,4 +1,31 @@
 // ? Librerias
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
+import { ButtonProps } from './Button'
 
-export const StyledButton = styled.button``
+const Button = styled.button<ButtonProps>`
+  ${({
+    primary: isPrimary
+  }) => {
+    const defaultStyle = `
+      display: inline-block;
+      min-width: 135px;
+      color: #8bcee8;
+      font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+      line-height: 1.5;
+      font-weight: 400;
+      text-align: center;
+      vertical-align: middle;
+      text-decoration: none;
+      outline: none;
+      border: solid 1px;
+      transition: all 0.3s linear;
+      cursor: pointer;
+    `
+
+    const styles = `
+    ${defaultStyle}
+    ${isPrimary && primary}
+    `
+
+    return styles
+  }}

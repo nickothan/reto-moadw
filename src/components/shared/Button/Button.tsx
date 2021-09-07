0c5d805cafc82b5bc0b2c446b08107af85d94573
Nickothan <s1nicolasrg@gmail.com>
2021-09-07 17:02:56 -0500

    [MDW-6] create component and props from Button.tsx

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -2,6 +2,17 @@
 import { ReactNode } from 'react'
 import { StyledButton } from './styles'
 
-const Button = () => <StyledButton>Button</StyledButton>
+//? Propiedades del coponente boton
+export type ButtonProps = {
+  type?: 'button' | 'submit' | 'reset' | undefined
+  children?: ReactNode | undefined
+  primary?: boolean
+}
+
+const Button = ({ type = 'button', children, ...arg }: ButtonProps) => (
+  <StyledButton type={type} {...arg}>
+    {children}
+  </StyledButton>
+)
 
 export default Button

6bd919cd6b6faef9a4047bace75ce724dd42cad1
Nickothan <s1nicolasrg@gmail.com>
2021-09-08 13:33:55 -0500

    MDW-25 update styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,5 +2,8 @@ import styled from 'styled-components/macro'
 
 export const StyledHeader = styled.header`
   background-color: #000;
+  margin: auto;
   padding: 20px 0;
+  width: 60%;
+  border-bottom: 2px solid red;
 `

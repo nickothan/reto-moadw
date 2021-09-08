816afce0336b44ab67dab93d01693416414b26fd
Nickothan <s1nicolasrg@gmail.com>
2021-09-08 16:58:28 -0500

    MDW-16-17 import components button and select

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,13 @@
 //* libraries
 import { StylesHome } from './styles'
 
+import Button from '../shared/Button'
+import Select from '../shared/Select'
+
 export default function Home() {
   return (
     <StylesHome>
-      <h1>Home</h1>
+      <h1>Donation</h1>
     </StylesHome>
   )
 }

e29f0d2c18eb4bb4e81c330a8b1ce39c3924cc6b
Nickothan <38267333+nickothan@users.noreply.github.com>
2021-09-08 14:48:40 -0500

    MDW-3 header (#6)
    * [MDW-3] Create folder and files base
    
    * [MDW-12] Logo is center from a header
    
    * MDW-25 update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from './components/Header'
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <h1>Reto MoaDW</h1>
     </div>
   )
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { StyledHeader } from './styles'
+
+import { ReactComponent as Logo } from '../assets/Logo.svg'
+
+const Header = () => (
+  <StyledHeader>
+    <Logo />
+  </StyledHeader>
+)
+
+export default Header

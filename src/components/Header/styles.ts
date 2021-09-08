f7cae0a8fa949a1ad4ba5c43170cfa72dfc1372b
Nickothan <38267333+nickothan@users.noreply.github.com>
2021-09-08 14:50:09 -0500

    MDW-3 Header (#3)
    * Add styled-components
    
    * Cleaning files
    
    * add logo svg
    
    * Update styled components from typescript
    
    * [MDW-3] Create folder and files base
    
    * [MDW-12] Logo is center from a header
    
    * MDW-25 update styles
    
    * MDW-26 Create global styles (#2)
    
    * Mdw-1 Button (#4)
    
    * Create folder and files base
    
    * [MDW-6] create component and props from Button.tsx
    
    * [MOW-7] styles from Button
    
    * [MDW-8] Animation ffrom Button
    
    * [MDW-6-7] update styles and component props
    
    * Mdw-2 Select (#5)
    
    * Create folder and files base
    
    * [MDW-9-10] update component and styles from select

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro'
+
+export const StyledHeader = styled.header`
+  background-color: #000;
+  margin: auto;
+  padding: 20px 0;
+  width: 60%;
+  border-bottom: 2px solid red;
+`
---
+++ src/components/shared/Button/styles.ts
@@ -0,0 +1,26 @@
+// ? Librerias
+import styled from 'styled-components/macro'
+
+export const StyledButton = styled.button`
+  display: inline-block;
+  min-width: 135px;
+  color: #8bcee8;
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+  font-weight: 600;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
+  background-color: transparent;
+  outline: none;
+  border: solid 1px;
+  border-radius: 25px;
+  transition: all 0.3s linear;
+  cursor: pointer;
+
+  &:hover {
+    background: linear-gradient(to right, #8bcee8, #9ec3f8);
+    color: #000;
+    border: none;
+  }
+`

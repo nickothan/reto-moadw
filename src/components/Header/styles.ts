5916b2707a927a92e71ace52b60ab43bdc064fd0
Nickothan <s1nicolasrg@gmail.com>
2021-09-07 21:35:32 -0500

    [MDW-12] Logo is center from a header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,6 @@
 import styled from 'styled-components/macro'
 
-export const StyledHeader = styled.header``
+export const StyledHeader = styled.header`
+  background-color: #000;
+  padding: 20px 0;
+`

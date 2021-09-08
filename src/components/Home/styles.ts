7138c1d13ca6a51fdd40bd36ed9e0818db7177c3
Nickothan <s1nicolasrg@gmail.com>
2021-09-08 18:10:22 -0500

    MDW-28 Update styles from user in home

--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -8,7 +8,7 @@ export const StylesHome = styled.div`
   color: white;
 
   h1 {
-    font-size: 2.5rem;
+    font-size: 3rem;
   }
 
   & > div:first-child {
@@ -24,5 +24,35 @@ export const StylesHome = styled.div`
     margin: 30px;
   }
 `
-export const StylesUser = styled.li``
-export const Donation = styled.div``
+export const StylesUser = styled.li`
+  margin: 10px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  align-items: center;
+  padding: 30px 20px;
+  border: 2px solid salmon;
+  border-radius: 8px;
+
+  h3 {
+    max-width: 200px;
+    font-size: 2rem;
+  }
+
+  img {
+    background-color: salmon;
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+  }
+`
+
+export const Donation = styled.div`
+  p {
+    font-weight: 600;
+  }
+
+  span {
+    font-size: 1.9rem;
+    font-weight: 700;
+  }
+`

b90ca47180cdd3501023ee4258d19b80e818fd09
Nickothan <s1nicolasrg@gmail.com>
2021-09-08 17:48:38 -0500

    MDW-30 Update Component user in home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,10 @@
 //* libraries
-import { StylesHome } from './styles'
+import { StylesHome, StylesUser, Donation } from './styles'
 
 import Button from '../shared/Button'
 import Select from '../shared/Select'
+
+const User = () => ()
 
 export default function Home() {
   return (
@@ -11,7 +13,11 @@ export default function Home() {
         <h1>Donations</h1>
         <Select />
       </div>
-      <ul></ul>
+      <ul>
+        <User />
+        <User />
+        <User />
+      </ul>
       <div>
         <Button>Prev</Button>
         <Button>Next</Button>

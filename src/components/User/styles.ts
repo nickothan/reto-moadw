7e594862ce3aa65f41e303c3a6466fc64ae724d3
Nickothan <s1nicolasrg@gmail.com>
2021-09-10 11:23:03 -0500

    MDW-20-33 Import button and create layout

--- src/components/User/styles.ts
+++ src/components/User/styles.ts
@@ -2,5 +2,10 @@
 import styled from 'styled-components/macro'
 
 export const UserContainer = styled.div`
+  padding: 20px 0;
+  margin: auto;
+  width: 65%;
+  height: 100%;
   color: white;
 `
+export const Donations = styled.div``
